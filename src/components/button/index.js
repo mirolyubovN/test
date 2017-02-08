1ef6af546c683d95d059d64d911d69ab2b169b4e
mirolyubovN <mirolyubov_n@yahoo.co.uk>
2017-02-08 10:33:08 +0000

    changed index js

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -13,7 +13,7 @@ export default class Button extends Component {
 		return (
 			<div>
 				<button onClick={clickFunction}>
-					Display Weather pls
+					Display Weather you shit!
 				</button>
 			</div>
 		);
