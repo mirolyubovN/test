cfd17a6ca3f258c15884a868f59a64bbe72cbd53
ec15143 <macbookpro@macbookpros-mbp.home>
2017-02-15 14:09:45 +0000

    rearranged the data, changed background image to black to see the changes more easily

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -25,31 +25,35 @@ export default class Iphone extends Component {
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "http://api.wunderground.com/api/6e014545bd8a1361/conditions/q/UK/Brighton.json";
+		var url = "http://api.wunderground.com/api/6e014545bd8a1361/conditions/q/UK/London.json";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
 			success : this.parseResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		})
-		// once the data grabbed, hide the button
-		//this.setState({ display: false });
-		this.fetchWeaklyWeatherData();
-	}
 
-	// a call to fetch weather data via wunderground
-	fetchWeaklyWeatherData = () => {
-		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "http://api.wunderground.com/api/6e014545bd8a1361/forecast10day/q/UK/Brighton.json";
+		url = "http://api.wunderground.com/api/6e014545bd8a1361/forecast10day/q/UK/London.json";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
 			success : this.parseWeaklyResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		})
+
+		/*url = "http://api.wunderground.com/api/6e014545bd8a1361/hourly/q/UK/London.json";
+		$.ajax({
+			url: url,
+			dataType: "jsonp",
+			success : this.parseHourlyResponse,
+			error : function(req, err){ console.log('API call failed ' + err); }
+		})*/
+
 		// once the data grabbed, hide the button
 		this.setState({ display: false });
 	}
+
+
 
 	// the main render method for the iphone component
 	render() {
@@ -58,6 +62,7 @@ export default class Iphone extends Component {
 
 		// display all weather data
 		/* input the wind speed in km/h*/
+
 		return (
 			<div class={ style.container }>
 				<div class={ style.header }>
@@ -68,17 +73,18 @@ export default class Iphone extends Component {
 
 				<div class={ style.details }><span class={ style.conditions  }>{ this.state.wind }</span><br/><span class={ style.conditions  }>{ this.state.feels }</span><br/><span class={ style.conditions  }> { this.state.test } </span></div>
 				<div class={ style.details }> <br/><br/>
-				<table align="center">
+				<table align="center" >
 				<tr>
-					<td><span class={ style.conditions  }> { this.state.m3 } </span><span class={ style.conditions  }> { this.state.d1 } </span></td><td><span class={ style.conditions  }> { this.state.d2 } </span></td><td><span class={ style.conditions  }> { this.state.d3 } </span></td><td><span class={ style.conditions  }> { this.state.d4 } </span></td><td><span class={ style.conditions  }> { this.state.d5 } </span></td><td><span class={ style.conditions  }> { this.state.d6 } </span></td><td><span class={ style.conditions  }> { this.state.d7} </span></td>
+					<td align="left"><span class={ style.conditions  }> { this.state.m3 } </span></td><td><span class={ style.conditions  }> { this.state.d1 } </span></td><td><span class={ style.conditions  }> { this.state.d2 } </span></td><td><span class={ style.conditions  }> { this.state.d3 } </span></td><td><span class={ style.conditions  }> { this.state.d4 } </span></td><td><span class={ style.conditions  }> { this.state.d5 } </span></td><td><span class={ style.conditions  }> { this.state.d6 } </span></td><td><span class={ style.conditions  }> { this.state.d7} </span></td>
 				</tr>
 				<tr>
-					<td><span class={ style.conditions  }> { this.state.m1 } </span><span class={ style.conditions  }> { this.state.w1 } </span></td><td><span class={ style.conditions  }> { this.state.w2 } </span></td><td><span class={ style.conditions  }> { this.state.w3 } </span></td><td><span class={ style.conditions  }> { this.state.w4 } </span></td><td><span class={ style.conditions  }> { this.state.w5 } </span></td><td><span class={ style.conditions  }> { this.state.w6 } </span></td><td><span class={ style.conditions  }> { this.state.w7 } </span></td>
+					<td align="left"><span class={ style.conditions  }> { this.state.m1 } </span></td><td><span class={ style.conditions  }> { this.state.w1 } </span></td><td><span class={ style.conditions  }> { this.state.w2 } </span></td><td><span class={ style.conditions  }> { this.state.w3 } </span></td><td><span class={ style.conditions  }> { this.state.w4 } </span></td><td><span class={ style.conditions  }> { this.state.w5 } </span></td><td><span class={ style.conditions  }> { this.state.w6 } </span></td><td><span class={ style.conditions  }> { this.state.w7 } </span></td>
 				</tr>
 				<tr>
-					<td><span class={ style.conditions  }> { this.state.m2 } </span><span class={ style.conditions  }> { this.state.l1 } </span></td><td><span class={ style.conditions  }> { this.state.l2 } </span></td><td><span class={ style.conditions  }> { this.state.l3 } </span></td><td><span class={ style.conditions  }> { this.state.l4 } </span></td><td><span class={ style.conditions  }> { this.state.l5 } </span></td><td><span class={ style.conditions  }> { this.state.l6 } </span></td><td><span class={ style.conditions  }> { this.state.l7 } </span></td>
+					<td align="left"><span class={ style.conditions  }> { this.state.m2 } </span></td><td><span class={ style.conditions  }> { this.state.l1 } </span></td><td><span class={ style.conditions  }> { this.state.l2 } </span></td><td><span class={ style.conditions  }> { this.state.l3 } </span></td><td><span class={ style.conditions  }> { this.state.l4 } </span></td><td><span class={ style.conditions  }> { this.state.l5 } </span></td><td><span class={ style.conditions  }> { this.state.l6 } </span></td><td><span class={ style.conditions  }> { this.state.l7 } </span></td>
 				</tr>
 				</table>
+
 				 </div>
 				<div class= { style_iphone.container }>
 					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData }/ > : null }
@@ -111,7 +117,7 @@ export default class Iphone extends Component {
 	    var minweather = [];
 	    var mes1 = "Min";
 	    var mes2 = "Max";
-	    var mes3 = "Day:";
+	    var mes3 = "Day";
 	    for (var i=0; i<8; i++) {
 		days.push(parsed_json['forecast']['simpleforecast']['forecastday'][i]['date']['weekday_short']);
 		maxweather.push(parsed_json['forecast']['simpleforecast']['forecastday'][i]['high']['celsius'] );
@@ -146,5 +152,26 @@ export default class Iphone extends Component {
 
 		});
 		}
+		/*parseWeaklyResponse = (parsed_json) => {
+	    var hour = [];
+	    var temp = [];
+	    var icon= [];
+	    var pop = [];
+	    var mes1 = "Min";
+	    var mes2 = "Max";
+	    var mes3 = "Day:";
+	    for (var i=0; i<25; i++) {
+		hours.push(parsed_json['hourly_forecast'][i]['FCTTIME']['hour']);
+		temp.push(parsed_json['hourly_forecast'][i]['temp']['metric'] );
+		icon.push(parsed_json['hourly_forecast'][i]['icon'] );
+		pop.push(parsed_json['hourly_forecast'][i]['pop'] );
+		}
+		// set states for fields so they could be rendered later on
+		this.setState({
+
+
+
+		});
+		}*/
 }
 
